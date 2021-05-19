b9c28246fbf9dad5e75f36b6741da1f2b96cca4d
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-20 08:43:43 +0900

    % rails db:migrateを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,19 +15,19 @@ class User < ApplicationRecord
     validates :birth_day
   end
 
-  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]\z/
-  validates :first_name, format: { with: NAME_REGEX }
-  validates :last_name, format: { with: NAME_REGEX }
+  VALID_NAME_REGEX = Regexp.new(/\A[ぁ-んァ-ヶ一-龥々ー]\z/)
+  validates :first_name, format: { with: VALID_NAME_REGEX }
+  validates :last_name, format: { with: VALID_NAME_REGEX }
 
-  NAME_KANA_REGEX = /\A[ァ-ヶー]\z/
-  validates :first_name_kana format: { with: NAME_KANA_REGEX }
-  validates :last_name_kana format: { with: NAME_KANA_REGEX }
+  VALID_NAME_KANA_REGEX = Regexp.new(/\A[ァ-ヶー]\z/)
+  validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }
 
-  EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
-  validates :email, uniqueness: true, format: { with: EMAIL_REGEX }
+  VALID_EMAIL_REGEX = Regexp.new(/\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i)
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i
-  validates :encrypted_password format: { with: PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = Regexp.new(/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i)
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
 
 
 
