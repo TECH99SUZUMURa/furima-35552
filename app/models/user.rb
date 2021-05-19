0529b9e47473f07de2067b8d91a4f5241b4daf2d
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-20 08:06:25 +0900

    バリデーションに正規表現を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,20 @@ class User < ApplicationRecord
     validates :birth_day
   end
 
-  validates :email, uniqueness: true
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]\z/
+  validates :first_name, format: { with: NAME_REGEX }
+  validates :last_name, format: { with: NAME_REGEX }
+
+  NAME_KANA_REGEX = /\A[ァ-ヶー]\z/
+  validates :first_name_kana format: { with: NAME_KANA_REGEX }
+  validates :last_name_kana format: { with: NAME_KANA_REGEX }
+
+  EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
+  validates :email, uniqueness: true, format: { with: EMAIL_REGEX }
   
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i
+  validates :encrypted_password format: { with: PASSWORD_REGEX }
+
+
+
 end
