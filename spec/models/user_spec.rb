e036ffa8c9271481d0eb24e589ce3aaae50fdcfe
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-21 07:52:36 +0900

    テストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context 'ユーザー新規登録ができるとき' do
+      it 'nickname, email, password, password_confirmation, first_name, last_name, first_name_kana, last_name_kana, phone_numが全て存在すれば登録できる'
+      end
+
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      end
+
+      it 'ユーザー本名は全角（漢字・ひらがな・カタカナ）の入力があれば登録できる' do
+      end
+
+      it 'フリガナは、全角（カタカナ）の入力があれば登録できる' do
+      end
+    end
+
+    context 'ユーザー新規登録ができないとき' do
+      it 'nicknameが空の状態では登録できない' do
+      end
+
+      it 'emailが空の状態では登録できない' do
+      end
+
+      it 'emailには重複したデータは登録できない' do
+      end
+
+      it 'emailには@が含まれていないデータは登録できない' do
+      end
+
+      it 'passwordが空の状態では登録できない' do
+      end
+
+      it 'passwordが存在しても、password_confirmationが空では登録できない' do
+      end
+
+      it 'passwordおよびpassword_conformationには半角英数字混合ではないデータは登録できない' do
+      end
+
+      it 'passwordが5文字以下では登録できない' do
+      end
+    end
+  end
+
+  describe 'ユーザーログイン' do
+    context 'ユーザーログインができるとき' do
+      it 'emailとpasswordが存在すれば登録できる' do
+      end
+    end
+
+    context 'ユーザーログインができないとき' do
+      it 'emailが空の状態では登録できない' do
+      end
+
+      it 'passwordが空の状態では登録できない' do
+      end
+    end
+  end
 end
