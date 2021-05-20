de878ce6a5cbc338942a0bc63b9161fe61834dbc
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-20 14:07:57 +0900

    正規表現をコメントアウト（単体テスト前に再検証）

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,23 +11,22 @@ class User < ApplicationRecord
     validates :last_name
     validates :first_name_kana
     validates :last_name_kana
-    validates :nickname
     validates :birth_day
   end
 
-  VALID_NAME_REGEX = Regexp.new(/\A[ぁ-んァ-ヶ一-龥々ー]\z/)
-  validates :first_name, format: { with: VALID_NAME_REGEX }
-  validates :last_name, format: { with: VALID_NAME_REGEX }
+  # VALID_NAME_REGEX = Regexp.new(/\A[ぁ-んァ-ヶ一-龥々ー]\z/)
+  # validates :first_name, format: { with: VALID_NAME_REGEX }
+  # validates :last_name, format: { with: VALID_NAME_REGEX }
 
-  VALID_NAME_KANA_REGEX = Regexp.new(/\A[ァ-ヶー]\z/)
-  validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
-  validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  #VALID_NAME_KANA_REGEX = Regexp.new(/\A[ァ-ヶー]\z/)
+  #validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  #validates :last_name_kana, format: { with: VALID_NAME_KANA_REGEX }
 
-  VALID_EMAIL_REGEX = Regexp.new(/\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i)
-  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  # VALID_EMAIL_REGEX = Regexp.new(/\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i)
+  # validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
-  VALID_PASSWORD_REGEX = Regexp.new(/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i)
-  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
+  #VALID_PASSWORD_REGEX = Regexp.new(/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i)
+  #validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX }
 
 
 
