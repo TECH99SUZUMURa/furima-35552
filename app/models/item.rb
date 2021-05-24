932fe8fe581b4136c486046b3d7bd97dec4ea4a5
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-24 11:25:30 +0900

    ItemモデルとActive_Hashファイルのアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash: :Associations: :ActiveRecordExtentions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :estimated_shipping_date
 end
