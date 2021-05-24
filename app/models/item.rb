f24213d85d131bc7a5763c8a52e491a6f474911f
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-24 12:13:09 +0900

    Itemモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,19 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :estimated_shipping_date
+
+  with_options presence: true do
+    validates :item_name
+    validates :text
+    validates :price
+  end
+
+  with_options numericalty: {other_than: 1} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :estimated_shipping_date_id
+  end
+
 end
