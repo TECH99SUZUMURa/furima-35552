9aa59363d3bbd75ef87d21242b881a7f306dc4de
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-25 15:21:23 +0900

    FactoryBotの記述を修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name                   {Faker::Name.initials(number: 3)}
+    price                       {300 + Faker::Number.number(digits: 3)}
+    text                        {Faker::Lorem.sentence}
+    category_id                 {Faker::Number.between(from: 2, to: 11)}
+    condition_id                {Faker::Number.between(from: 2, to: 7)}
+    shipping_charge_id          {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id               {Faker::Number.between(from: 2, to: 48)}
+    estimated_shipping_date_id  {Faker::Number.between(from: 2, to: 4)}
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test-image.png'), filename: 'test-image.png')
+    end
   end
 end
