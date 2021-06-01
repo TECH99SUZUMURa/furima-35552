f726e052080f5557335eb3152bbfd065a9ae6cb4
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-01 11:34:59 +0900

    Formオブジェクトにバリデーションを記述

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,13 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_num, :user_id
+
+  with_optioons presence: true do
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :manucipality, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid"}
+    validates :address, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー0-9\-]+\z/, message: "is invalid"}
+    validates :phone_num, format: {with: /\A\d{11}\z/, message: "is invalid. Not include hyphen(-)"}
+  end
 end
