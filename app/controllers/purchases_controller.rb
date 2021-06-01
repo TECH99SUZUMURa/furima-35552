c1fda45509e247a85c4d0583941d4b959dd601fe
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-01 12:00:47 +0900

    purchaseコントローラーのcreateアクションとプライベートメソッドを定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,20 @@
 class PurchasesController < ApplicationController
   def index
   end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render template "items/index"
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :manucipality, :address, :building_name).merge(user_id: current_user.id)
+  end
 end
