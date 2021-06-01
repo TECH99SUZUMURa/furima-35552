49e18269db43280c15040d9cf39688a5f9b84580
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-01 10:45:10 +0900

    マイグレーションファイルを生成し、マイグレーションを実行

--- db/migrate/20210601004324_create_addresses.rb
+++ db/migrate/20210601004324_create_addresses.rb
@@ -1,6 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
+      t.string :postal_code,    null: false
+      t.integer :prefecture_id, null: false
+      t.string :manucipality,   null: false
+      t.string :address,        null: false
+      t.string :building_name
+      t.string :phone_num,      null: false
+      t.references :purchase,   null: false, foreign_key: true
 
       t.timestamps
     end
