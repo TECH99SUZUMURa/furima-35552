4e413cd996a510b193b30a86d6cca822058255a5
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-02 18:20:04 +0900

    バリデーションに引っ掛かった際のエラーメッセージを編集

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,10 +6,10 @@ class PurchaseAddress
     validates :user_id
     validates :item_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1}
     validates :manucipality, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid"}
     validates :address, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー0-9\-]+\z/, message: "is invalid"}
-    validates :phone_num, format: {with: /\A\d{11}\z/, message: "is invalid. Not include hyphen(-)"}
+    validates :phone_num, format: {with: /\A\d{11}\z/, message: "is invalid. Only 11 half-width numbers without hyphen(-)"}
   end
 
   def save
