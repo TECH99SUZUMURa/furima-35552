b31f0224280c33c54a8b2621a4e5b0919c84d442
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-02 14:21:41 +0900

    ログイン中のユーザーでも売却済みの商品の購入ページのURLを直接入力してもページ遷移できないようにした

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,14 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index_owner, only: :index
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index_owner, only: [:index, :create]
+  before_action :move_to_index_sold_out, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       @purchase_address.save
@@ -24,8 +24,15 @@ class PurchasesController < ApplicationController
     params.require(:purchase_address).permit(:postal_code, :prefecture_id, :manucipality, :address, :building_name, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
-  def move_to_index_owner
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index_owner
     redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def move_to_index_sold_out
+    redirect_to root_path if @item.purchase.present?
   end
 end
