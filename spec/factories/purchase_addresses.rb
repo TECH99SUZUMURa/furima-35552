d86c81b8e0b92ef09c815e458f5dd47aa3be2179
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-02 18:19:34 +0900

    テスト用のデータをFactoryBotで設定

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    postal_code {'123-4567'}
+    prefecture_id {2}
+    manucipality {'大田区あイ町'}
+    address {'あイ宇1-1-1'}
+    building_name {'コーポきら山田 B-206'}
+    phone_num {"09012345678"}
   end
 end
