fb2775f8287752734b2c36a2cae2f456ebf76319
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-06-04 17:19:50 +0900

    issue修正指示①　市区町村および番地の正規表現を削除

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,8 +7,8 @@ class PurchaseAddress
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1 }
-    validates :manucipality, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid' }
-    validates :address, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー0-9\-]+\z/, message: 'is invalid' }
+    validates :manucipality
+    validates :address
     validates :phone_num, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Only 11 half-width numbers without hyphen(-)' }
     validates :token
   end
